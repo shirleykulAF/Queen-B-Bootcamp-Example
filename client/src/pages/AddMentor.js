b2eaf041138098ba46691314c9352320cd31f46e
Eden-Ismah-Moshe <eden12012@gmail.com>
2024-09-15 14:07:58 +0300

    add frontend of home screen

---
+++ client/src/pages/AddMentor.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddMentor = () => {
+  return <div>AddMentor</div>;
+};
+
+export default AddMentor;
---
+++ client/src/pages/ViewAllMentors.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ViewAllMentors = () => {
+  return <div>ViewAllMentors</div>;
+};
+
+export default ViewAllMentors;
