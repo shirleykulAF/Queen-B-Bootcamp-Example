1736db4912fa3cbd5dcd9dfe07ed9bf23dc5b540
Inbar <inbar.lahat@post.idc.ac.il>
2024-09-19 23:26:14 +0300

    adjusments in carousel

--- client/src/pages/WelcomePage.js
+++ client/src/pages/WelcomePage.js
@@ -1,23 +1,29 @@
 // display the welcom page to the web
-import React from 'react';
-import Carousel from '../components/Carousel';
-import { Link } from 'react-router-dom';
-import './WelcomePage.css';
-
+import React from "react";
+import Carousel from "../components/Carousel";
+import { Link } from "react-router-dom";
+import "./WelcomePage.css";
 
 const WelcomePage = () => {
     return (
-        <div className="welcome-container">
-            <header className="welcome-header">
-                <h1 className="welcome-title">Where women in tech come to learn and connect</h1>
-                <p className="welcome-subtitle">Join a community of like-minded women in tech, find mentors, and grow your skills together.</p>
-
-                {/* Get Started button - navigates to sign up */}
-                <Link to="/signup" className="welcome-button">Get Started</Link>
-
-                <Carousel />
-            </header>
-        </div>
+        <>
+            <div className="welcome-container">
+                <header className="welcome-header">
+                    <h1 className="welcome-title">
+                        Where women in tech come to learn and connect
+                    </h1>
+                    <p className="welcome-subtitle">
+                        Join a community of like-minded women in tech, find mentors, and
+                        grow your skills together.
+                    </p>
+                    <Link to="/signup" className="welcome-button">
+                        Get Started
+                    </Link>
+                    
+                    <Carousel />
+                </header>
+            </div>
+        </>
     );
 };
 
