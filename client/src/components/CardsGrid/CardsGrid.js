d23ad9029373de5da8b4b3d07d6e23837f4bb7f1
AmberWein <amberw.dev@gmail.com>
2024-09-26 10:35:56 +0300

    Add search button to look for mentors by name

--- client/src/components/CardsGrid/CardsGrid.js
+++ client/src/components/CardsGrid/CardsGrid.js
@@ -1,28 +1,6 @@
-// import React from 'react';
-// import Card from '../Card/Card';
-// import './CardsGrid.css'
-// // import { mentorsList } from '../../assets/Data/MentorsList';
-
-
-// const CardsGrid = ({mentorsList}) => {
-//     console.log(mentorsList);
-//     return (
-//         <div className='cardsGrid'>
-//         {mentorsList && mentorsList.map((mentor, index) => (
-//         // <Card key={index} mentor={mentor} />
-//            <Card mentorName={mentor.name} mentorBrief={mentor.about} mentorPhotoPath={mentor.profile_photo} />
-//         ))}
-//         </div>
-//     );
-// };
-
-// export default CardsGrid;
-
-
 import React from 'react';
 import Card from '../Card/Card';
 import './CardsGrid.css'
-// import { mentorsList } from '../../assets/Data/MentorsList';
 
 
 const CardsGrid = ({mentorsList}) => {
@@ -30,7 +8,6 @@ const CardsGrid = ({mentorsList}) => {
     return (
         <div className='cardsGrid'>
         {mentorsList && mentorsList.map((mentor, index) => (
-        // <Card key={index} mentor={mentor} />
            <Card mentorName={mentor.name} mentorBrief={mentor.about} mentorPhotoPath={mentor.profile_photo} />
         ))}
         </div>
